2da714387a1c52cbb6db8054cd39d7a00c082336
Virjanand <virjanand@virjanand.com>
2020-07-18 06:29:16 +0200

    Checking checkbox adds strikethrough

--- lib/widgets/task_tile.dart
+++ lib/widgets/task_tile.dart
@@ -1,12 +1,24 @@
 import 'package:flutter/material.dart';
 
 class TaskTile extends StatelessWidget {
+  final bool isChecked;
+  final String taskTitle;
+  final Function checkboxCallback;
+
+  const TaskTile({this.isChecked, this.taskTitle, this.checkboxCallback});
+
   @override
   Widget build(BuildContext context) {
     return ListTile(
-      title: Text('Task'),
+      title: Text(
+        'This is a Task',
+        style: TextStyle(
+            decoration: isChecked ? TextDecoration.lineThrough : null),
+      ),
       trailing: Checkbox(
-        value: false,
+        activeColor: Colors.lightBlueAccent,
+        value: isChecked,
+        onChanged: checkboxCallback,
       ),
     );
   }
